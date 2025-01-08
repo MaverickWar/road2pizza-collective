0139dc6890ea80786f8304acc76679c34ab7937e
MaverickWar <rich@revuit.net>
2025-01-08 14:11:51 +0000

    Update NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
     <div className="container mx-auto p-6">
-      <h1 className="text-2xl font-bold">404 - Page Not Found</h1>
+      <h1 className="text-2xl font-bold">OH DEAR! Would you believe it? It's a 404 - Page Not Found</h1>
     </div>
   );
 }

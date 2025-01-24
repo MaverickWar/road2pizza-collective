bb59847748ed1c231c229cf021e7b7e4130616b6
MaverickWar <rich@revuit.net>
2025-01-24 10:44:34 +0000

    Update PizzaStyleHeader.tsx

--- src/components/pizza/PizzaStyleHeader.tsx
+++ src/components/pizza/PizzaStyleHeader.tsx
@@ -9,9 +9,7 @@ interface PizzaStyleHeaderProps {
 
 export const PizzaStyleHeader = ({ title, description, onSubmitRecipe }: PizzaStyleHeaderProps) => {
   return (
-    <div className="text-center py-6">
-      <h1 className="text-4xl font-bold text-textLight mb-3">{title}</h1>
-      <p className="text-xl text-textLight mb-4">{description}</p>
+ 
       <Button 
         onClick={onSubmitRecipe}
         className="bg-accent hover:bg-accent-hover text-white"
